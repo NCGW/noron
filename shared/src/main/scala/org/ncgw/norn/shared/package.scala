50a25e5b4cf04c076afd2fbfd68c8c4312dae438
“shuai1126” <1042820762@qq.com>
2020-05-30 19:09:02 +0800

    。

---
+++ shared/src/main/scala/org/ncgw/norn/shared/package.scala
@@ -0,0 +1,26 @@
+package org.ncgw.norn
+
+/**
+  * Author: Administrator
+  * Date: 2020/3/23/023
+  * Time: 16:05
+  */
+package object shared {
+
+  trait Request
+
+  trait Response {
+    val errCode: Int
+    val msg: String
+  }
+
+  case class CommonRsp(
+    errCode: Int = 0,
+    msg: String = "ok"
+  ) extends Response
+
+  def ErrorRsp(errCode:Int, msg: String) = CommonRsp(errCode, msg)
+
+  val SuccessRsp = CommonRsp(0, "0k")
+
+}
