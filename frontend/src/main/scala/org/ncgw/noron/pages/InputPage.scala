28e346b57a8792a89e34b1c8930010216345dea9
“shuai1126” <1042820762@qq.com>
2020-05-30 18:45:34 +0800

    。

--- frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
+++ frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
@@ -1,15 +1,45 @@
 package org.ncgw.noron.pages
 
 import mhtml._
-import org.scalajs.dom
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import io.circe.generic.auto._
+import io.circe.syntax._
+import scala.language.postfixOps
+import org.ncgw.noron.utils.Http
+import org.ncgw.noron.Routes
+import org.ncgw.norn.shared.CommonProtocol.{ContentInputRsp, ContentInputReq}
+import concurrent.ExecutionContext.Implicits.global
+import org.scalajs.dom
 
 object InputPage {
 
-  def app:xml.Node = {
+  def inputContent() : Unit = {
+
+    val data = ContentInputReq(0l, "").asJson.noSpaces
+
+    Http.postJsonAndParse[ContentInputRsp](Routes.ContentRoutes.inputContent, data).map {
+      case Right(rsp) =>
+        if (rsp.errCode == 0) {
+
+        } else {
+
+        }
+
+      case Left(e) =>
+        println(s"get roomList with ${e}")
+
+    }
+  }
+
+  def app: xml.Node = {
     println("123")
-    <div>123</div>
+    <div>
+      <div style="text-align: center;">
+        <input id="shuru" class="input-a" ></input>
+      </div>
+      <div style="display: flex; margin-top: 20%;">
+        <button class="voice-button"><img src="/noron/static/img/语音输入.png" style="width:200px;"></img></button>
+        <button class="voice-button"><img src="/noron/static/img/图片.png" style="width:188px;"></img></button>
+      </div>
+    </div>
   }
 }
