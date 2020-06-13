7fec198b64bf7316039f578ad6eb7baf363fe2eb
“shuai1126” <1042820762@qq.com>
2020-06-13 18:59:43 +0800

    页面更新

--- frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
+++ frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
@@ -16,7 +16,7 @@ import org.scalajs.dom.raw.FormData
 
 object InputPage {
 
-  val test = List(("上午九点","打卡"),("上午十点","晨会"),("下午三点","汇报"))
+  val test = List(("上午九点","打卡","日程"),("上午十点","晨会","心愿"),("下午三点","汇报",""))
   def inputContent() : Unit = {
 
     val data = ContentInputReq(0l, "").asJson.noSpaces
@@ -53,24 +53,38 @@ object InputPage {
   def app: xml.Node = {
     println("123")
     <div>
-      <div style="text-align: center;">
-        <input id="shuru" class="input-a" ></input>
-      </div>
-      {
-      test.map{ v =>
-          <div class="plane">
-            <div class="plane-time">{v._1}</div>
-            <div class="plane-todo">{v._2}</div>
-          </div>
-
-      }
-      }
-      <div style="display: flex; margin-top: 20%;">
+      <div class="type">
+        <div class="text-input">文字输入</div>
         <button class="voice-button"><img src="/noron/static/img/语音输入.png" style="width:100px;"></img></button>
         <div class="upload">
           <img src="/noron/static/img/图片.png" style="width:84px; height:84px"></img>
           <input type="file" class="pic-input" name="file" onchange={(e: dom.Event) => upload(e.target.asInstanceOf[Input])}></input>
         </div>
+      </div>
+
+      <div style="margin-left: 10%;">
+        <div>
+          <lable class="label-type">时间</lable>
+          <input id="shuru" class="input-a" ></input>
+        </div>
+        <div>
+          <lable class="label-type">任务</lable>
+          <input id="shuru" class="input-a" ></input>
+        </div>
+        <div>
+          <lable class="label-type">类型</lable>
+          <input id="shuru" class="input-a" ></input>
+        </div>
+      </div>
+      <div class="plane">
+        <div class="plane-time">上午九点</div>
+        <div class="plane-todo">打卡</div>
+        <div class="plane-todo">日程</div>
+      </div>
+
+      <div style="margin: 10%;text-align: center;">
+        <button class="confirm">上传</button>
+        <button class="confirm" style="background: #FF0000;">确认</button>
       </div>
     </div>
   }
