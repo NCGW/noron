23feecccff7b9418cc631003810aba9bab8ee053
“shuai1126” <1042820762@qq.com>
2020-07-04 18:21:08 +0800

    .

--- frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
+++ frontend/src/main/scala/org/ncgw/noron/pages/InputPage.scala
@@ -4,6 +4,7 @@ import mhtml._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.ncgw.noron.shared.InputProtocol.{AddTaskReq, uploadSuccessRsp}
+import org.ncgw.noron.shared.SpeechToTextProtocol._
 import org.ncgw.noron.shared.SuccessRsp
 import scala.language.postfixOps
 import org.ncgw.noron.utils.Http
@@ -45,7 +46,7 @@ class InputPage(userId : Long) {
       val imgs = fileName.toString().drop(4).dropRight(1)
       val data = AddTaskReq(10001l,startTime.toLong, endTime.toLong, content, taskType, imgs).asJson.noSpaces
 
-      Http.postJsonAndParse[SuccessRsp](Routes.ContentRoutes.addTask, data).map {
+      Http.postJsonAndParse[SuccessRsp](Routes.TaskList.addTask, data).map {
         case Right(rsp) =>
           if (rsp.errCode == 0) {
             JsFunc.alert("任务上传成功！")
@@ -63,13 +64,47 @@ class InputPage(userId : Long) {
 
   }
 
+  def uploadContent() : Unit = {
 
+    val task = document.getElementById("type").asInstanceOf[Input].value
+    val taskType = task match {
+      case "日程" => 0
+      case "任务" => 1
+      case "心愿" => 2
+    }
+
+      val data = GetParseTextReq(taskType, "/Users/chenzhishuai/IdeaProjects/noron/audio/agenda1.wav").asJson.noSpaces
+
+      Http.postJsonAndParse[GetParseTextRsp](Routes.ContentRoutes.getParseText, data).map {
+        case Right(rsp) =>
+          if (rsp.errCode == 0) {
+            println("111" + rsp.startTime, rsp.endTime, rsp.duringTime, rsp.ddl, rsp.taskType, rsp.taskContent)
+            JsFunc.alert("任务上传成功！")
+            println("任务上传成功")
+          } else {
+            JsFunc.alert("任务上传失败！")
+            println(rsp.msg)
+          }
+        case Left(e) =>
+          println(s"get roomList with ${e}")
+      }
+  }
+
+val start = Var(false)
   def app: xml.Node = {
     println("123")
     <div>
       <div class="type">
         <div class="text-input">文字输入</div>
-        <button class="voice-button"><img src="/noron/static/img/语音输入.png" style="width:100px;"></img></button>
+        {
+        start.map{
+          a =>
+            <button class="voice-button" onclick={()=> if(a == false) {start := true; JsFunc.alert("开始录音")} else
+            {start := false; JsFunc.alert("结束录音，请输入任务类型并提交")} }>
+              <img src="/noron/static/img/语音输入.png" style="width:100px;"></img>
+            </button>
+        }
+        }
         {
         fileName.map{
           img =>
@@ -105,16 +140,14 @@ class InputPage(userId : Long) {
           <input id="type" class="input-a" ></input>
         </div>
       </div>
-      <div class="plane">
-        <div class="plane-time">上午九点</div>
-        <div class="plane-todo">打卡</div>
-        <div class="plane-todo">日程</div>
+      <div style="margin-top: 5%;text-align: center;">
+        <button class="confirm" onclick={()=>inputContent()}>上传文字</button>
+        <button class="confirm" style="background: rgba(255, 64, 75, 1);" onclick={()=>uploadContent()}>上传语音</button>
+      </div>
+      <div style="text-align: center;">
+        <button class="confirm" style="background: rgba(22, 213, 133, 1);">解析确认</button>
       </div>
 
-      <div style="margin-top: 10%;text-align: center;">
-        <button class="confirm" onclick={()=>inputContent()}>上传</button>
-        <button class="confirm" style="background: rgba(255, 64, 75, 1);">确认</button>
-      </div>
     </div>
   }
 }
